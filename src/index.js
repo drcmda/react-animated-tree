f3d88c95a06e184d1de91190d032e49ae05fd546
Paul Henschel <drcmda@gmail.com>
2018-05-13 20:34:55 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,20 +3,45 @@ import PropTypes from 'prop-types'
 import { Spring, config, animated } from 'react-spring'
 import * as Icons from './icons'
 
-const Contents = ({ children, ...styles }) => (
-  <animated.div
-    style={{
-      ...styles,
-      willChange: 'transform, opacity, height',
-      marginLeft: 6,
-      padding: '4px 0px 0px 14px',
-      borderLeft: '1px dashed rgba(255,255,255,0.4)',
-    }}>
+const styles = {
+  tree: {
+    position: 'relative',
+    padding: '4px 0px 0px 0px',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflowX: 'hidden',
+    width: '100%',
+    verticalAlign: 'middle',
+  },
+  toggle: {
+    width: '1em',
+    height: '1em',
+    marginRight: 10,
+    cursor: 'pointer',
+    verticalAlign: 'middle',
+  },
+  contents: {
+    willChange: 'transform, opacity, height',
+    marginLeft: 6,
+    padding: '4px 0px 0px 14px',
+    borderLeft: '1px dashed rgba(255,255,255,0.4)',
+  },
+  type: {
+    textTransform: 'uppercase',
+    fontFamily: 'monospace',
+    fontSize: '0.6em',
+    verticalAlign: 'middle',
+  },
+}
+
+const Contents = ({ children, ...style }) => (
+  <animated.div style={{ ...style, ...styles.contents }}>
     {children}
   </animated.div>
 )
 
 export default class Tree extends React.PureComponent {
+  static defaultProps = { open: false, visible: true, canHide: false }
   static propTypes = {
     open: PropTypes.bool,
     visible: PropTypes.bool,
@@ -24,21 +49,23 @@ export default class Tree extends React.PureComponent {
     content: PropTypes.node,
     springConfig: PropTypes.func,
   }
-  static defaultProps = { open: false, visible: true, canHide: false }
 
   constructor(props) {
     super()
     this.state = { open: props.open, visible: props.visible, immediate: false }
   }
+
   toggle = () =>
     this.props.children &&
     this.setState(state => ({ open: !state.open, immediate: false }))
+
   toggleVisibility = () => {
     this.setState(
       state => ({ visible: !state.visible, immediate: true }),
       () => this.props.onClick && this.props.onClick(this.state.visible)
     )
   }
+
   componentWillReceiveProps(props) {
     this.setState(state => {
       return ['open', 'visible'].reduce(
@@ -55,50 +82,19 @@ export default class Tree extends React.PureComponent {
     const Icon =
       Icons[`${children ? (open ? 'Minus' : 'Plus') : 'Close'}SquareO`]
     return (
-      <div
-        style={{
-          position: 'relative',
-          padding: '4px 0px 0px 0px',
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-          overflowX: 'hidden',
-          width: '100%',
-          verticalAlign: 'middle',
-          ...style,
-        }}
-        className="treeview">
+      <div style={{ ...styles.tree, ...style }} className="treeview">
         <Icon
           className="toggle"
-          style={{
-            width: '1em',
-            height: '1em',
-            marginRight: 10,
-            opacity: children ? 1 : 0.3,
-            cursor: 'pointer',
-            verticalAlign: 'middle',
-          }}
+          style={{ ...styles.toggle, opacity: children ? 1 : 0.3 }}
           onClick={this.toggle}
         />
-        <span
-          style={{
-            marginRight: type ? 10 : 0,
-            textTransform: 'uppercase',
-            fontFamily: 'monospace',
-            fontSize: '0.6em',
-            verticalAlign: 'middle',
-          }}>
+        <span style={{ ...styles.type, marginRight: type ? 10 : 0 }}>
           {type}
         </span>
         {canHide && (
           <Icons.EyeO
             className="toggle"
-            style={{
-              width: '1em',
-              height: '1em',
-              marginRight: 10,
-              opacity: visible ? 1 : 0.4,
-              verticalAlign: 'middle',
-            }}
+            style={{ ...styles.toggle, opacity: visible ? 1 : 0.4 }}
             onClick={this.toggleVisibility}
           />
         )}
